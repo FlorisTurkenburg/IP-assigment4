e4e2bd41e70af8a70d9b759ed2db4903ade15d2a
Floris <sk8_floris@hotmail.com>
2015-10-12 21:00:05 +0200

    just went thru the steps in the README

---
+++ paper/config.h
@@ -0,0 +1,9 @@
+#ifndef __ASSIGNMENT4__
+#define __ASSIGNMENT4__
+#define WEB_BASEPHP "/~mstamat"
+#define WEB_BASECGI "/~mstamat/cgi-bin"
+#define HOTELGW_ADDRESS "localhost"
+#define HOTELGW_PORT 3211
+#define PAPER_ADDRESS "localhost"
+#define PAPER_PORT 3222
+#endif
